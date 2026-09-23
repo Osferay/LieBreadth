4a9b7fe58c7447f388bbebeeffe2dce5f7065339
Osferay <oscar00ayala@gmail.com>
2026-09-23 13:34:42 +0200

    Updated documentations

--- gap/breadth.gd
+++ gap/breadth.gd
@@ -3,28 +3,28 @@
 #! @ChapterTitle Preface
 
 #! In this package we compute the breadth of Lie algebras.
-#! This package also include functions to compute covered Lie algebras of maximal class using a proces called
-#! inflation, this is described by Caranti, Mattarei, and Newman <Cite Key="inflation" />.
+#! This package also includes functions to compute covered Lie algebras of maximal class using a process called
+#! inflation, which is described by Caranti, Mattarei, and Newman <Cite Key="inflation" />.
 	
 #! @Section Introduction
-#! Given a Lie algebra $L$, we define its lower central series as $L = L^1 > L^2 > \dots$, where $L^{i+1} = L^i L$.
-#! The algebra $L$ is nilpotent if there exists $c \in \N$ such that $L^{c+1} = {0}$, and the minimal such $c$ 
-#! is called the class $\mathrm{cl}(L)$ of $L$. For a nilpotent Lie algebra $L$, the type of $L$
-#! is the vector $(d_1,\dots,d_c)$, where $d_i = \dim L^i$. A nilpotent Lie algebra $L$ is said to be of maximal class if
+#! Given a finite-dimensional algebra $L$, we define its lower central series as $L = L^1 > L^2 > \dots$, where $L^{i+1} = L^i L$.
+#! The algebra $L$ is nilpotent if there exists $c \in \mathbb{N}$ such that $L^{c+1} = \{0\}$, and the minimal such $c$ 
+#! is called the class $\mathrm{cl}(L)$ of $L$. For a nilpotent algebra $L$, the type of $L$
+#! is the vector $(d_1,\dots,d_c)$, where $d_i = \dim L^i$. A nilpotent algebra $L$ is said to be of maximal class if
 #! the type of $L$ is $(2,1,\dots,1)$. The centralizer of $x \in L$ is the subspace of $L$ 
 #! defined by $C_L(x) = \{a \in L \mid ax = 0\}$. For an algebra $L$, we define 
 #! $$ \mathrm{br}(L) = \max \{ \mathrm{br}(x) \mid x \in L\}, \;\; \mbox{ where } \;\; \mathrm{br}(x) = \dim(L) - \dim(C_L(x)). $$
 #! The class-breadth conjecture asserts that $\mathrm{cl}(L) \leq \mathrm{br}(L)+1$ for an algebra $L$.
-#! This holds for nilpotent Lie algebras over infinite fields and for nilpotent associative algebras over arbitrary fields. 
+#! The conjecture holds for nilpotent Lie algebras over infinite fields and for nilpotent associative algebras over arbitrary fields. 
 #!
-#! Let $L$ be a Lie algebra over a field $K$, and let $B = \{ b_1, \ldots, b_n \}$  be a basis of $L$. 
-#! The multiplication in $L$ is described by structure constants
-#! $$ b_i \cdot b_j = \sum_{k=1}^n c_{ijk} b_k \quad 1\leq i,j \leq n. $$
+#! Let $L$ be a Lie algebra over a field $K$, and let $B = \{ b_1, \ldots, b_n \}$ be a basis of $L$. 
+#! The multiplication in $L$ is described by structure constants defined by
+#! $$ b_i \cdot b_j = \sum_{k=1}^n c_{ijk} b_k \quad\text{ for } 1\leq i,j \leq n. $$
 #! For each $k$, write $C_k = (c_{ijk})_{1 \leq i,j \leq n}$ for the $n \times n$ matrix over $K$ 
-#! with entries $c_{ijk}$, these matrices are the structure matrices of $L$. For $x = x_1 b_1 + \ldots + x_n b_n \in L$,
+#! with entries $c_{ijk}$, and these matrices are the structure matrices of $L$. For $x = x_1 b_1 + \ldots + x_n b_n \in L$,
 #! let $\overline{x} = (x_1, \ldots, x_n) \in K^n$ denote the coefficient vector of $x$. 
 #! Then $C_k \overline{x}^{tr}$ is a column vector over $K$. We write $M_B(x)$ for the 
-#! $n \times n$ matrix over $K$ whose $k$-th column is $C_k \overline{x}^{tr}$, this is the adjoint matrix of $L$ with respect to the basis $B$. 
+#! $n \times n$ matrix over $K$ whose $k$-th column is $C_k \overline{x}^{\mathrm{tr}}$, which is the adjoint matrix of $L$ with respect to the basis $B$. 
 
 ### Lie.gi
 #! @Chapter Lie algebras and breadth
@@ -49,11 +49,11 @@ DeclareProperty(  "IsOfMaximalClass", IsLieNilpotent );
 #! @Arguments L
 DeclareAttribute( "StructureMatrices", IsLieAlgebra);
 #! @Description
-#! Returns the elements of the basis Lie algebra $L$ that are contained in the center of $L$.
+#! Returns the basis elements of the Lie algebra $L$ that are contained in the center of $L$.
 #! @Arguments L
 DeclareAttribute( "BasisLieCenter", IsLieAlgebra );
 #! @Description
-#! Returns the elements of the basis Lie algebra $L$ that are contained in the derived subalgebra of $L$.
+#! Returns the basis elements of the Lie algebra $L$ that are contained in the derived subalgebra of $L$.
 #! @Arguments L
 DeclareAttribute( "BasisLieDerived", IsLieAlgebra );
 #! @Description
@@ -76,37 +76,37 @@ DeclareInfoClass( "InfoLieBreadth" );
 #! @Arguments L
 DeclareAttribute( "LieBreadth", IsLieNilpotent );
 #! @Description
-#! Returns whether the Lie algebra $L$ holds the class-breadth conjecture or not.
+#! Returns whether the class-breadth conjecture holds for the Lie algebra $L$.
 #! @Arguments L
 DeclareProperty(  "IsTrueClassBreadth", IsLieNilpotent );
 #! @Description
-#! Computes the breadth of the T-groupo $G$.
+#! Computes the breadth of the $\mathcal{T}$-group $G$.
 #! @Arguments G
 DeclareGlobalFunction( "TGroupBreadth" );
 
-### Infaltion.gi
+### Inflation.gi
 #! @Section Inflation of Lie algebras
 
-#! A grading for a Lie algebra $L$ is a decomposition L = $\bigoplus_{i=1}^n L_i$ that respects the Lie bracket,
-#! **i.e.** $[L_i,L_j]\subseteq L_{i+j}$.}. Any nilpotent Lie algebras can be graded by taking $L_i = \gamma_i(L)/\gamma_{i+1}(L)$.
+#! A grading for a Lie algebra $L$ is a decomposition $L = \bigoplus_{i=1}^n L_i$ that respects the Lie bracket,
+#! **i.e.** $[L_i,L_j]\subseteq L_{i+j}$. All nilpotent Lie algebras can be graded by taking $L_i = \gamma_i(L)/\gamma_{i+1}(L)$.
 #! Let $L$ be a nilpotent Lie algebra of maximal class. The two-step centralizers are the sets 
 #! $C_i = C_{L_1}(L_i) = \{x \in L_1 \mid [x,L_i] = 0 \}$ for all $2\leq i \leq c$.
-#! Let $\mathcal{C}=\{C_i\}\setminus L_1$, we say that a Lie algebra of maximal class is covered if the 
+#! Let $\mathcal{C}=\{C_i\}\setminus L_1$. We say that a Lie algebra of maximal class is covered if the 
 #! set $\mathcal{C}$ consist of all one-dimensional subspaces of $L_1$.
 #!
-#! Let $L$ be a graded Lie algebra $L = \bigoplus_{i=1}^n L_i$ over $K=\F_q$ for some prime power $q=p^n$. 
+#! Let $L = \bigoplus_{i=1}^n L_i$ be a graded Lie algebra over $K=\mathbb{F}_q$ for some prime power $q=p^n$. 
 #! Consider the field extension $A=K[\varepsilon]/\langle \varepsilon^p \rangle$ which is a vector space over $K$ of 
-#! dimension $p$. The algebra $A$ is an associative, commutative and has a unit. The algebra $L\otimes A$ over $K$ 
-#! defined by $[x\otimes a, y\otimes b] = [x,y]\otimes ab$ is a graded Lie algebra. Let $M$ be a maximal 
+#! dimension $p$. The algebra $A$ is an associative, commutative algebra with a unit. The algebra $L\otimes A$ over $K$, 
+#! defined by $[x\otimes a, y\otimes b] = [x,y]\otimes ab$, is a graded Lie algebra. Let $M$ be a maximal 
 #! ideal of $L$ and consider the Lie subalgebra $M^\uparrow = M\otimes A$. Let $D$ be a derivation of $L$ 
-#! of degree 1. Define $D^\uparrow$ via: $D^\uparrow(x\otimes \varepsilon^i)=D(x)\otimes \varepsilon^i$, this is a 
-#! derivation of $M^\uparrow$ of degree 1. Define the derivation $E\in \Der M^\uparrow$ by 
+#! of degree 1. Define $D^\uparrow$ by: $D^\uparrow(x\otimes \varepsilon^i)=D(x)\otimes \varepsilon^i$, which is a 
+#! derivation of $M^\uparrow$ of degree 1. Define the derivation $E\in \operatorname{Der} M^\uparrow$ by 
 #! $E(x\otimes \varepsilon^i) = D^\uparrow(x\otimes\varepsilon^i\cdot\varepsilon^{p-1})+1\otimes\partial_\varepsilon(\varepsilon^i)$.
-#! Let $s\in L_1\setminus M$, take $D=\mathrm{ad}_s$, and extend it naturally to 
-#! $M^\uparrow$. Denote $E_{s^\prime}$ as previously. The **inflation** ${}^{M}L$ of $L$ at $M$ by 
+#! For $s\in L_1\setminus M$, take $D=\mathrm{ad}_s$, and extend it naturally to 
+#! $M^\uparrow$. Denote $E_{s^\prime}$ the derivation defined above. The **inflation** ${}^{M}L$ of $L$ at $M$ by 
 #! $s\in L_1\setminus M$ is the graded Lie algebra obtained as an extension of $M^\uparrow$ by an element 
 #! $s^\prime$, which is the extension of $s$ that induces the derivation $E_{s^\prime}$, that is,
-#! $$ [x\otimes a, s^\prime] = E_{s^\prime}(x\otimes a) = \mathrm{ad}_{s^\prime}(x\otimes\varepsilon^i)+x\otimes\partial_\varepsilon(\varepsilon^i). $$
+#! $$ [x\otimes a, s^\prime] = E_{s^\prime}(x\otimes a) = \operatorname{ad}_{s^\prime}(x\otimes\varepsilon^i)+x\otimes\partial_\varepsilon(\varepsilon^i). $$
 
 #! @Description
 #! Info class for the functions of the inflation of Lie algebras.
@@ -116,7 +116,7 @@ DeclareInfoClass( "InfoInflation" );
 #! @Arguments L
 DeclareAttribute( "LieNilpotentGrading", IsLieNilpotent );
 #! @Description
-#! Computes the two step centralizers of the Lie algebra $L$.
+#! Computes the two-step centralizers of the Lie algebra $L$.
 #! @Arguments L
 DeclareAttribute( "LieTwoStepCentralizers", IsLieNilpotent );
 #! @Description
@@ -124,15 +124,15 @@ DeclareAttribute( "LieTwoStepCentralizers", IsLieNilpotent );
 #! @Arguments L
 DeclareProperty(  "IsLieCovered", IsLieNilpotent );
 #! @Description
-#! For a finite field $F$ computes the polynomial algebra $F[x]$.
+#! For a finite field $F$, computes the polynomial algebra $F[x]$.
 #! @Arguments F
 DeclareAttribute( "PolynomialAlgebra", IsField and IsFinite );
 #! @Description
-#! For $n=2,3,4$ computes the covered Lie algebras of maximal class using the polynomial algebra of dimension $n$.
-#! For n=3 one can directly load the Lie algebra by reading the file "inflation_3.g".
+#! For $n=2,3,4$, computes the covered Lie algebras of maximal class using the polynomial algebra of dimension $n$.
+#! For $n=3$, one can directly load the Lie algebra by reading the file \texttt{inflation\_3.g}.
 #! @Arguments n
 DeclareAttribute( "LieCoveredInflated", IsInt );
 #! @Description
-#! Given a covered Lie algebras of maximal class $L$ computes the minimal quotient that not holds the class-breadth conjecture.
+#! Given a covered Lie algebras of maximal class $L$, computes the minimal quotient for which the class-breadth conjecture does not hold.
 #! @Arguments L
 DeclareAttribute( "LieMinimalQuotientClassBreadth", IsLieAlgebra );
